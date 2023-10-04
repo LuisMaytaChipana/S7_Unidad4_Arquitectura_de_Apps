6e5ace86a81c9ea139d47f538c610d1ab24f97e4
Luis Angel <luis.mayta@unmsm.edu.pe>
2023-10-04 15:16:18 -0500

    Unidad 4 - Navegacion y arquitectura de la app: Android Studio

---
+++ ViewModelyEstadoenCompose/app/src/main/java/com/example/viewmodelyestadoencompose/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.viewmodelyestadoencompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.example.viewmodelyestadoencompose.ui.GameScreen
+import com.example.viewmodelyestadoencompose.ui.theme.ViewModelYEstadoEnComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ViewModelYEstadoEnComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    GameScreen()
+                }
+            }
+        }
+    }
+}
